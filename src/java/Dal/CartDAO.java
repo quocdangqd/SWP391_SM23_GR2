0964b9984d81e1fac006334ad3239670b55b52eb
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2023-06-14 09:02:04 +0700

    Update CartDAO.java

--- src/java/Dal/CartDAO.java
+++ src/java/Dal/CartDAO.java
@@ -4,6 +4,8 @@
  */
 package Dal;
 
+import Model.Cart;
+
 /**
  *
  * @author PC
@@ -14,39 +16,41 @@ public class CartDAO extends ConnectMySQL {
         try {
             String sqlSelect = "DELETE FROM `swp`.`cart` WHERE (`cartID` = ?);";
             pstm = connection.prepareStatement(sqlSelect);
-            pstm.setString(1, cartID);
-            rs = pstm.executeQuery();
-            while (rs.next()) {
-                return true;
-            }
+            pstm.setInt(1, Integer.parseInt(cartID));
+            pstm.execute();
+            return true;
         } catch (Exception e) {
             System.out.println("DeleteCartByID: " + e);
         }
         return false;
     }
-//    public boolean AddCart(Q user) {
-//        try {
-//            String sqlSelect = "insert into swp.user( username, password, user_roleID, name, age, user_sexID, address, phone_number, avatar, register_code, email, status) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-////            user.setUserID(String.valueOf(GetUserIndex()));
-//            pstm = connection.prepareStatement(sqlSelect);
-////            pstm.setInt(1, Integer.parseInt(user.getUserID()));
-//            pstm.setString(1, user.getUsername());
-//            pstm.setString(2, user.getPassword());
-//            pstm.setInt(3, Integer.parseInt(user.getUser_roleID()));
-//            pstm.setString(4, user.getName());
-//            pstm.setInt(5, Integer.parseInt(user.getAge()));// fix in model
-//            pstm.setInt(6, Integer.parseInt(user.getUser_sexID()));
-//            pstm.setString(7, user.getAddress());
-//            pstm.setString(8, user.getPhone_number());
-//            pstm.setString(9, user.getAvatar());
-//            pstm.setString(10, user.getRegister_code());
-//            pstm.setString(11, user.getEmail());
-//            pstm.setInt(12, Integer.parseInt(user.getStatus()));
-//            pstm.execute();
-//            return true;
-//        } catch (Exception e) {
-//            System.out.println("AddUser: " + e);
-//        }
-//        return false;
-//    }
+
+    public boolean AddCart(Cart cart) {
+        try {
+            String sqlSelect = "INSERT INTO `swp`.`cart` (`price`, `quantity`, `productID`, `userID`) VALUES (?, ?, ?, ?);";
+            pstm = connection.prepareStatement(sqlSelect);
+            pstm.setFloat(1, Float.parseFloat(cart.getPrice()));
+            pstm.setInt(2, Integer.parseInt(cart.getQuantity()));
+            pstm.setInt(3, Integer.parseInt(cart.getProductID()));// 
+            pstm.setInt(4, Integer.parseInt(cart.getUserID()));
+            pstm.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("AddCart: " + e);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CartDAO cartDAO = new CartDAO();
+        Cart cart = new Cart();
+
+        cart.setPrice("2003");
+        cart.setQuantity("10");
+        cart.setProductID("1");
+        cart.setUserID("1");
+        cartDAO.AddCart(cart);
+        cartDAO.DeleteCartByID("20");
+//        System.out.println(cart.getPrice());
+    }
 }
