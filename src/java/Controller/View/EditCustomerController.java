c6b5dd98f8b2f61569dd855f65dc106935356682
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2023-07-20 20:33:30 +0700

    Update EditCustomerController.java

--- src/java/Controller/View/EditCustomerController.java
+++ src/java/Controller/View/EditCustomerController.java
@@ -4,17 +4,27 @@
  */
 package Controller.View;
 
+import Dal.AccountDao;
+import Model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+//import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author PC
  */
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+        maxFileSize = 1024 * 1024 * 10, // 10MB
+        maxRequestSize = 1024 * 1024 * 50) // 50MB
 public class EditCustomerController extends HttpServlet {
 
     /**
@@ -31,15 +41,49 @@ public class EditCustomerController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EditCustomerController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EditCustomerController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            HttpSession session = request.getSession();
+            User user = (User) session.getAttribute("user");
+            if (request.getParameter("saveChanges") != null) {
+                AccountDao accountDao = new AccountDao();
+                String name = request.getParameter("name");
+                String phone_number = request.getParameter("phone_number");
+                String email = request.getParameter("email");
+                String address = request.getParameter("address");
+                String age = request.getParameter("age");
+                String user_sexID =request.getParameter("user_sexID"); 
+                boolean checkEmail = accountDao.checkExistAccountByEmailAndUserID(email,user.getUserID());
+                if (checkEmail) {
+                    request.setAttribute("Message", "Email người dùng đã tồn tại!");
+                    request.getRequestDispatcher("userProfile.jsp").forward(request, response);
+                    return;
+                }
+                if (!request.getPart("avatar").getSubmittedFileName().isEmpty()) {
+                    String UploadPath = request.getServletContext().getRealPath("view/UserImage");
+                    out.print(UploadPath);
+                    File file = new File(UploadPath);
+                    if (!file.exists()) {
+                        file.mkdir();
+                    }
+                    Part photoPart = request.getPart("avatar");
+                    String fileName = photoPart.getSubmittedFileName();
+                    photoPart.write(UploadPath + "/" + fileName);
+                    user.setAvatar(fileName);
+                }
+                user.setUser_sexID(user_sexID);
+                user.setName(name);
+                user.setPhone_number(phone_number);
+                user.setEmail(email);
+                user.setAddress(address);
+                user.setAge(age);
+                session.setAttribute("user", user);
+                
+                accountDao.EditUser(user);
+                request.getRequestDispatcher("userProfile.jsp").forward(request, response);
+            } else if (user != null) {
+                request.getRequestDispatcher("userProfile.jsp").forward(request, response);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
         }
     }
 
@@ -70,6 +114,7 @@ public class EditCustomerController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+
     }
 
     /**
