11e221b377ace1e20fbd45e68f1f063041293b15
NGUYEN VAN DUC <ducnvhe160331@fpt.edu.vn>
2023-07-20 16:47:52 +0700

    Merge branch 'main' into Backup

--- src/java/Controller/Manager/ManagerDeleteProduct.java
+++ src/java/Controller/Manager/ManagerDeleteProduct.java
@@ -1,33 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-
-package Controller.Manager;
-
-import Dal.ManagerDAO;
-import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public class ManagerDeleteProduct extends HttpServlet {
-    
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-    throws ServletException, IOException {
-        ManagerDAO dao = new ManagerDAO();
-        String id = req.getParameter("id");
-        dao.deleteProduct(id);
-        req.getRequestDispatcher("ManagerProductController").forward(req, resp);
-    } 
-
-    
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-    throws ServletException, IOException {
-        
-    }
-
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+// */
+//
+//package Controller.Manager;
+//
+//import Dal.ManagerDAO;
+//import java.io.IOException;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//public class ManagerDeleteProduct extends HttpServlet {
+//    
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//    throws ServletException, IOException {
+//        ManagerDAO dao = new ManagerDAO();
+//        String id = req.getParameter("id");
+//        dao.deleteProduct(id);
+//        req.getRequestDispatcher("ManagerProductController").forward(req, resp);
+//    } 
+//
+//    
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+//    throws ServletException, IOException {
+//        
+//    }
+//
+//}
